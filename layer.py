a5fa999a7fa5abaa766e6f97c70353238b06c97d
imserge <sergiu.smw@gmail.com>
2026-06-04 21:53:55 +0300

    added sequential class for better handling of layers

--- layer.py
+++ layer.py
@@ -14,9 +14,38 @@ class Layer:
         if self.activation:
             return self.activation(ret)
         return ret 
+    
+class Sequential:
+    def __init__(self, m_input:list[float], layers:list[Layer]):
+        self.m_input = np.array(m_input)
+        self.layers = layers
+        
+    def feedforward(self):
+        if(len(self.layers) == 0):
+            raise Exception("You must have at least one layer.")
+        first = self.layers[0]
+        out = first(self.m_input)
+        for i in range(1, len(self.layers)):
+            layer = self.layers[i]
+            out = layer(out)
+        return out                 
+                 
+                 
+         
 
 m_input = [1, 2, 5]
-x1 = Layer(32, 3, activation=activations.relu)(m_input)
-x2 = Layer(64, 32, activation=activations.relu)(x1)
-output = Layer(5, 64, activation=activations.softmax)(x2)
-print(output)
+OUTPUT_SIZE = 5
+
+# x1 = Layer(32, 3, activation=activations.relu)(m_input)
+# x2 = Layer(64, 32, activation=activations.relu)(x1)
+# output = Layer(5, 64, activation=activations.softmax)(x2)
+# print(output)
+
+
+net = Sequential(m_input, [
+    Layer(32, 3, activation=activations.relu),
+    Layer(64, 32, activation=activations.relu),
+    Layer(OUTPUT_SIZE, 64, activation=activations.softmax)
+])
+
+print(net.feedforward())
